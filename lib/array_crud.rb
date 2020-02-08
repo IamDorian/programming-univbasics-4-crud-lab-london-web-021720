1bacb362a68cf33c2dc44263137d170be49d49a3
Dorian Calin <dorin.m.calin@gmail.com>
2020-02-08 15:56:35 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   array = ["element1","element2","element3","element4"]
-  array.push
+  array.push("element5")
 end
 
 def add_element_to_start_of_array(array, element)
